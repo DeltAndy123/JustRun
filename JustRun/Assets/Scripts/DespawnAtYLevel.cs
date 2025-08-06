0d2ee145e97788a69c702f608d0e871aae4c3a9c
DeltAndy123 <105518328+DeltAndy123@users.noreply.github.com>
2025-08-06 14:52:18 -0400

    Despawn objects under scene

---
+++ JustRun/Assets/Scripts/DespawnAtYLevel.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DespawnAtYLevel : MonoBehaviour
+{
+    // The Y level where the object destroys itself
+    public float yLevel;
+    // If false, despawns when Y level is below, if true, despawns if above
+    public bool despawnAbove;
+    
+    
+    private void Update()
+    {
+        print(transform.position.y);
+        if ((despawnAbove && transform.position.y > yLevel) || (!despawnAbove && transform.position.y < yLevel))
+        {
+            Destroy(gameObject);
+        } 
+    }
+}
--- JustRun/Assets/Scripts/Player.cs
+++ JustRun/Assets/Scripts/Player.cs
@@ -99,7 +99,7 @@ public class Player : MonoBehaviour
     }
     
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (IsOnGround())
         {
